7ead2b57130238f31ab19b0f3f073fa19745a3bc
neethupath <pathnee222gmail.com>
2023-04-01 12:04:38 -0400

    advanced

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers=(function (drivers)
+{
+    return ['Antonia','Nuru'];
+});
+const returnLastTwoDrivers=(function (drivers)
+{
+    return ['Amari','Mo'];
+});
+const selectingDrivers =[returnFirstTwoDrivers,returnLastTwoDrivers];
+const createFareMultiplier=(function (value)
+{
+return (function()
+{
+return value*value;
+});
+}
+);
+const fareDoubler=(function ( createFareMultiplier)
+{
+return createFareMultiplier*2;
+});
+const fareTripler=(function (createFareMultiplier)
+{
+return createFareMultiplier*3;
+});
+    function selectDifferentDrivers(drivers, task){
+        for(let i = 0; i < selectingDrivers.length; i++){
+          if(selectingDrivers[i] === task){
+            return selectingDrivers[i](drivers);
+          }
+        }
+      }
